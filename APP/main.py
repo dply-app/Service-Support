a280754c87d2bb13480ecb9c136fead3af9a88e0
All PGS <kms0219kms@naver.com>
2021-05-16 21:29:47 +0900

    Update main.py

--- APP/main.py
+++ APP/main.py
@@ -1,9 +1,20 @@
 from md_write import *
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import os
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class bugreport_tools(BaseModel):
     User_email : str
